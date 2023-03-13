0ae604206eeb0c0cc76166b5c689b432b036a0a2
Anton Balter <antbalter@gmail.com>
2023-03-13 19:25:30 +0200

    cw74 microservices

--- src/test/java/telran/monitoring/PulseProbeImitatorApplicationTests.java
+++ src/test/java/telran/monitoring/PulseProbeImitatorApplicationTests.java
@@ -1,10 +1,17 @@
 package telran.monitoring;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.stream.binder.test.OutputDestination;
+import org.springframework.cloud.stream.binder.test.TestChannelBinderConfiguration;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(TestChannelBinderConfiguration.class)
 class PulseProbeImitatorApplicationTests {
+	@Autowired
+	OutputDestination consumer;
 
 	@Test
 	void contextLoads() {
